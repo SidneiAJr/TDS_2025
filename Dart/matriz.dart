f137b03b47e1a44ecab60eaa1324b0d9762097ae
Sid Jr <sidalbertok@gmail.com>
2025-05-14 19:37:50 -0300

    Create matriz.dart

---
+++ Dart/matriz.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<List<int>> matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+
+  print(matriz[0][0]); // 1 (linha 0, coluna 0)
+  print(matriz[1][2]); // 6 (linha 1, coluna 2)
+}
