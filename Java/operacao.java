fa73317316b5532953976722842fa40c2fe32704
Sid Jr <sidalbertok@gmail.com>
2025-05-14 19:45:36 -0300

    Create operacao.java

---
+++ Java/operacao.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        double n1 = 5;
+        double n2 = 5.71;
+        double n3 = 7.71;
+        double soma = n1+n2+n3;
+        double d = 6.6;
+        double c= 4.5;
+        double divisao = d/c;
+        double mult = d*c;
+        double sub = d-c;
+        System.out.printf("Soma: %.2f \n",soma);
+        System.out.printf("Divisao %.2f\n", divisao);
+        System.out.printf("Multiplicao %.2f\n", mult);
+        System.out.printf("Subtracao %.2f\n", sub);
+    }
+}
