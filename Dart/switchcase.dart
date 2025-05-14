ea25fba780538502ee3ac86a81b36ca347357541
Sid Jr <sidalbertok@gmail.com>
2025-05-14 19:59:22 -0300

    Create switchcase.dart

---
+++ Dart/switchcase.dart
@@ -0,0 +1,14 @@
+void main() {
+  int opcao = 2;
+
+  switch (opcao) {
+    case 1:
+      print('Opção 1');
+      break;
+    case 2:
+      print('Opção 2');
+      break;
+    default:
+      print('Opção inválida');
+  }
+}
