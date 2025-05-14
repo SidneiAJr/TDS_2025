79032f5b8eef3658dc97e24eca46f99d15d93549
Sid Jr <sidalbertok@gmail.com>
2025-05-14 19:53:38 -0300

    Create booleanos.dart,3

---
+++ Dart/booleanos.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 1;
+  int b = 2;
+  bool teste1 = a>b;
+  bool teste2 = a==b;
+  bool teste3 = a!=b;
+  bool teste4 = a<b;
+  print(teste1);
+  print(teste2);
+  print(teste3);
+  print(teste4);
+}
