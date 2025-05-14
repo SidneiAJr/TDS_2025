94efc292a5a43fb53e5c86cf288b362a310216f3
Sid Jr <sidalbertok@gmail.com>
2025-05-14 17:18:50 -0300

    Create criarbanco.sql

---
+++ SQL/criarbanco.sql
@@ -0,0 +1,8 @@
+-- create a table
+CREATE TABLE informacao (
+  id INTEGER PRIMARY KEY AUTO INCREMENT,
+  name TEXT NOT NULL,
+  cor TEXT NOT NULL,
+  telefone NUMBER NOT NULL,
+  cpf NUMBER NOT NULL,
+);
