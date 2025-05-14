de0460027b52a092f1cb6dec0ec537bf0f693f78
Sid Jr <sidalbertok@gmail.com>
2025-05-14 19:39:56 -0300

    Create soma.java

---
+++ Java/soma.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+        double n1 = 5;
+        double n2 = 5.71;
+        double n3 = 7.71;
+        double soma = n1+n2+n3;
+        System.out.println(soma);
+    }
+}
