1997b4bf6c61094f1bd9e2b2a586b7ef6bfa10b2
Sid Jr <sidalbertok@gmail.com>
2025-05-16 16:06:38 -0300

    Update bancoteste2.sql

--- SQL/SQL-Exercicios Test/bancoteste2.sql
+++ SQL/SQL-Exercicios Test/bancoteste2.sql
@@ -1,12 +1,9 @@
-CREATE TABLE estoqueloja(
+CREATE TABLE estoqueloja (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   nome_produto TEXT NOT NULL,
   valor INTEGER NOT NULL,
   quant_estoque INTEGER NOT NULL,
-  fabricante TEXT NOT NULL, 
+  fabricante TEXT NOT NULL
 );
 INSERT INTO estoqueloja(nome_produto,valor,quant_estoque,fabricante)
 VALUES('PRODUTO A',100,50,'PRODUTOR A');
-
-
-
