f653e4fd55af92238bf94a899a8ea706a5225773
Sid Jr <sidalbertok@gmail.com>
2025-05-16 15:24:51 -0300

    Create bancoteste2.sql

---
+++ SQL/SQL-Exercicios Test/bancoteste2.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE estoqueloja(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nome_produto TEXT NOTNULL,
+  valor INTEGER NOTNULL,
+  quant_estoque INTEGER NOTNULL,
+  fabricante INTEGER NOTNULL, 
+);
+INSERT INTO nome_produto 
+INSERT INTO valor
+INSERT INTO quant_estoque
+INSERT INTO fabricante
+
+
+
